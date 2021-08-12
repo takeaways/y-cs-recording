85c22202937a616411a242e0464f905ea9e613e6
lucas.j <lucas.j@hpcnt.com>
2021-08-13 00:44:39 +0900

    Updates

---
+++ app.0cc90a7f.js
@@ -0,0 +1,211 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"YSF2":[function(require,module,exports) {
+"use strict";
+
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+const recordButton = document.querySelector(".record-button");
+const stopButton = document.querySelector(".stop-button");
+const playButton = document.querySelector(".play-button");
+const downloadButton = document.querySelector(".download-button");
+const previewView = document.querySelector("#preview");
+const recordingView = document.querySelector("#recording");
+let recorder;
+let recordedChunks = []; // let recordingStream;
+// handlers
+
+function handleStartRecording() {
+  return __awaiter(this, void 0, void 0, function* () {
+    try {
+      const stream = yield navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+      });
+      recordingView.srcObject = stream;
+      recording(stream);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+}
+
+function handleStropRecording() {
+  recordingView.srcObject //
+  .getTracks().forEach(t => t.stop());
+  recorder.stop();
+}
+
+function handlePlayRecording() {
+  const recordedBlob = new Blob(recordedChunks, {
+    type: "video/webm"
+  });
+  previewView.src = URL.createObjectURL(recordedBlob);
+  previewView.play();
+  previewView.controls = true;
+  downloadButton.href = previewView.src;
+  downloadButton.download = `recording_${new Date()}.webm`;
+} // events
+
+
+recordButton.addEventListener("click", handleStartRecording);
+stopButton.addEventListener("click", handleStropRecording);
+playButton.addEventListener("click", handlePlayRecording); // functions
+
+function recording(stream) {
+  recordedChunks = [];
+  recorder = new MediaRecorder(stream);
+
+  recorder.ondataavailable = e => recordedChunks.push(e.data);
+
+  recorder.start();
+}
+},{}]},{},["YSF2"], null)
+//# sourceMappingURL=/app.0cc90a7f.js.map
